f0df39e332185ee5100f3c8b3ee6fbf082453a53
Vivek Patel <vivekpatel0910@gmail.com>
2020-02-20 16:31:20 +0530

    reactjs boilerplate with webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      // {
+      //   test: /\.css/,
+      //   loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?minimize=true' }),
+      // },
+      // {
+      //   test: /\.scss/,
+      //   loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader?minimize=true!sass-loader' }),
+      // },
+      {
+        test: /\.(svg|png|jpg|jpeg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          limit: 10000,
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'],
+  },
+  devServer: {
+    port: 3000,
+    historyApiFallback: {
+      index: './build/index.html',
+    },
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ]
+};
